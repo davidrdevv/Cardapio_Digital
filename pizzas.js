3f5809168a93f1819c776a7c3831e732ed3c9072
davidrdev <davidribeiro.developer@gmail.com>
2025-06-20 05:45:44 -0300

    feat: estrutura inicial do projeto

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.00, sizes:['100g', '530g', '860g'], description:'Clássica e irresistível, com generosas fatias de mussarela derretida'},
+    {id:2, name:'Marguerita', img:'images/pizza2.png', price:25.00, sizes:['320g', '530g', '860g'], description:'Combinação perfeita de mussarela, tomate fresco e manjericão'},
+    {id:3, name:'Romana', img:'images/pizza3.png', price:22.00, sizes:['320g', '530g', '860g'], description:'Deliciosa mistura de mussarela, presunto, tomates frescos, azeitonas pretas e orégano'},
+    {id:4, name:'Vegetariana', img:'images/pizza4.png', price:24.00, sizes:['320g', '530g', '860g'], description:'Uma explosão de sabores frescos com vegetais selecionados'},
+    {id:5, name:'Siciliana', img:'images/pizza5.png', price:27.00, sizes:['320g', '530g', '860g'], description:'Rústica e saborosa, com aliche, alcaparras e azeitonas'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:28.00, sizes:['320g', '530g', '860g'], description:'Uma surpresa do chef com ingredientes selecionados'},
+    {id:7, name:'M&M', img:'images/pizza7.png', price:26.00, sizes:['320g', '530g', '860g'], description:'Uma doce tentação com M&M, perfeita para os amantes de sobremesas'}
+];
